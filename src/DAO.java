8b227f702e9ba3bc47ddd5ec098000a09dd97faf
flo4313 <fsmague@gmail.com>
2019-12-11 16:34:00 +0100

    Login view

--- src/DAO.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface DAO<T> {
-	public List<T> getAll();
-	public T get(int id);
-	public boolean save(T obj);
-	public boolean update(T obj);
-	public boolean delete(T obj);
-}
