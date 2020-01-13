646c8c5b60f31f309730d042b73dfaefa6e04ad3
PepitoSama <32514188+PepitoSama@users.noreply.github.com>
2020-01-13 20:37:13 +0100

    📜 Java Doc

--- src/bl/manager/GroupManager.java
+++ src/bl/manager/GroupManager.java
@@ -19,72 +19,115 @@ import model.domain.UserGroup;
  */
 public class GroupManager {
 
-    private static GroupManager instance = null;
-    private Group selected;
+	private static GroupManager instance = null;
+	private Group selected;
 
-    public Group getSelected() {
-	return selected;
-    }
-
-    public static GroupManager getInstance() {
-	if (instance == null) {
-	    instance = new GroupManager();
-	}
-	return instance;
-    }
-
-    public boolean createGroup(String groupName) {
-	// Create the Group
-	DAO<Group> dao = AbstractDAOFactory.getInstance().getGroupDAO();
-	Group g;
-	try {
-	    checkExisting(groupName);
-	} catch (Exception e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
-	g = new Group(groupName);
-	return dao.save(g);
-    }
-
-    public void checkExisting(String groupName) throws Exception {
-	DAO<Group> dao = AbstractDAOFactory.getInstance().getGroupDAO();
-	List<Group> groups = dao.getAll();
-	for (Group g : groups) {
-	    if (g.getGroupName().equals(groupName)) {
-		throw new Exception("Group Name already use");
-	    }
-	}
-    }
-
-    public List<Group> getGroupList(List<UserGroup> userGroupList) {
-	DAO<Group> dao = AbstractDAOFactory.getInstance().getGroupDAO();
-	List<Group> groupList = new ArrayList<>();
-	for (Group group : dao.getAll()) {
-	    for (UserGroup userGroup : userGroupList) {
-		if (group.getId() == userGroup.getIdGroup()) {
-		    groupList.add(group);
+	/**
+	 * Return, if created the GroupManager instance. Else, instantiate GroupManager
+	 * and return it.
+	 * 
+	 * @return GroupManager
+	 */
+	public static GroupManager getInstance() {
+		if (instance == null) {
+			instance = new GroupManager();
 		}
-	    }
+		return instance;
 	}
-	return groupList;
-    }
 
-    public void setSelected(Group group) {
-	this.selected = group;
-    }
+	/**
+	 * Return the selected Group
+	 * 
+	 * @return Group
+	 */
+	public Group getSelected() {
+		return selected;
+	}
 
-    public int getGroupId(String groupName) {
-	DAO<Group> dao = AbstractDAOFactory.getInstance().getGroupDAO();
-	Couple where = new Couple("name", groupName);
-	List<Couple> listWhere = new ArrayList<>();
-	listWhere.add(where);
-	List<Group> listGroup = dao.get(listWhere);
-	return listGroup.get(0).getId();
-    }
+	/**
+	 * Return the id of selected Group
+	 * @return int
+	 */
+	public int getSelectedGroupId() {
+		return getSelected().getId();
+	}
 
-    public int getSelectedGroupId() {
-	return getSelected().getId();
-    }
+	/**
+	 * Set the selected Group
+	 * 
+	 * @param selected Group
+	 */
+	public void setSelected(Group group) {
+		this.selected = group;
+	}
 
+	/**
+	 * Create a new Group
+	 * 
+	 * @param groupName String
+	 * @return true if the group was succefully created
+	 */
+	public boolean createGroup(String groupName) {
+		// Create the Group
+		DAO<Group> dao = AbstractDAOFactory.getInstance().getGroupDAO();
+		Group g;
+		try {
+			checkExisting(groupName);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		g = new Group(groupName);
+		return dao.save(g);
+	}
+
+	/**
+	 * Check if a Group name is not already used
+	 * 
+	 * @param groupName String
+	 * @throws Exception Exception("Group Name already use")
+	 */
+	public void checkExisting(String groupName) throws Exception {
+		DAO<Group> dao = AbstractDAOFactory.getInstance().getGroupDAO();
+		List<Group> groups = dao.getAll();
+		for (Group g : groups) {
+			if (g.getGroupName().equals(groupName)) {
+				throw new Exception("Group Name already use");
+			}
+		}
+	}
+
+	/**
+	 * Get all group from a List of UserGroup
+	 * 
+	 * @param userGroupList List<UserGroup>
+	 * @return List<Group>
+	 */
+	public List<Group> getGroupList(List<UserGroup> userGroupList) {
+		DAO<Group> dao = AbstractDAOFactory.getInstance().getGroupDAO();
+		List<Group> groupList = new ArrayList<>();
+		for (Group group : dao.getAll()) {
+			for (UserGroup userGroup : userGroupList) {
+				if (group.getId() == userGroup.getIdGroup()) {
+					groupList.add(group);
+				}
+			}
+		}
+		return groupList;
+	}
+
+	/**
+	 * Get a group id from it name
+	 * 
+	 * @param groupName String
+	 * @return int
+	 */
+	public int getGroupId(String groupName) {
+		DAO<Group> dao = AbstractDAOFactory.getInstance().getGroupDAO();
+		Couple where = new Couple("name", groupName);
+		List<Couple> listWhere = new ArrayList<>();
+		listWhere.add(where);
+		List<Group> listGroup = dao.get(listWhere);
+		return listGroup.get(0).getId();
+	}
 }
--- src/bl/manager/ListManager.java
+++ src/bl/manager/ListManager.java
@@ -5,13 +5,10 @@
  */
 package bl.manager;
 
-import java.sql.SQLException;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import model.dao.*;
 import model.domain.GroupList;
 import model.domain.products.GeneralProduct;
@@ -24,151 +21,167 @@ import model.domain.products.QuantifiedProduct;
  */
 public class ListManager {
 
-    public List<GroupList> getShoppingList(int id) {
-	DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
-	List<Couple> liste = new ArrayList<Couple>();
-	liste.add(new Couple("idGroup", Integer.toString(id)));
-	liste.add(new Couple("type", "S"));
-	return dao.get(liste);
-    }
+	private GroupList selected;
+	private static ListManager instance = null;
 
-    private GroupList selected;
-
-    public GroupList getSelected() {
-	return selected;
-    }
-
-    public void setSelected(GroupList selected) {
-	this.selected = selected;
-    }
-
-    public GroupList getFavoriteList(int id) {
-	DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
-	List<Couple> liste = new ArrayList<Couple>();
-	liste.add(new Couple("idGroup", Integer.toString(id)));
-	liste.add(new Couple("type", "F"));
-	return dao.get(liste).get(0);
-    }
-
-    private static ListManager instance = null;
-
-    public static ListManager getInstance() {
-	if (instance == null) {
-	    instance = new ListManager();
-	}
-	return instance;
-    }
-
-    public boolean addShopList(String name, int id) {
-	DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
-	return dao.save(new GroupList(0, id, name, new Date(), 'S'));
-    }
-
-    public boolean addFavorisList(int id) {
-	DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
-	return dao.save(new GroupList(0, id, "Favoris", new Date(), 'F'));
-    }
-
-    public boolean addSuggestList(int id) {
-	DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
-	return dao.save(new GroupList(0, id, "Suggest", new Date(), 'P'));
-    }
-
-    public boolean updateShopList(String name) {
-	DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
-	selected.setName(name);
-	return dao.update(selected);
-    }
-
-    public boolean removeList() {
-	DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
-	return dao.delete(selected);
-    }
-
-    public String getNameSelected() {
-	return selected.getName();
-    }
-
-    public void getBoughtProducts(GroupList selected) {
-	DAO<PricedProduct> dao = AbstractDAOFactory.getInstance().getPricedProductDAO();
-	List<Couple> liste = new ArrayList<Couple>();
-	liste.add(new Couple("idGroupList", Integer.toString(selected.getIdGroupList())));
-	selected.setBoughtProducts(dao.get(liste));
-    }
-
-    public int getGroupListId() {
-	return this.selected.getIdGroupList();
-    }
-
-    public boolean addProductsToShopList(List<GeneralProduct> selectedProducts) {
-	DAO<QuantifiedProduct> dao = AbstractDAOFactory.getInstance().getQuantifiedProductDAO();
-	boolean b = true;
-	for (GeneralProduct selectedProduct : selectedProducts) {
-	    List<Couple> liste = new ArrayList<Couple>();
-	    liste.add(new Couple("idGroupList", Integer.toString(selected.getIdGroupList())));
-	    liste.add(new Couple("idProduct", Integer.toString(selectedProduct.getIdProduct())));
-	    List<QuantifiedProduct> res = dao.get(liste);
-	    if (res.isEmpty()) {
-		if (!dao.save(new QuantifiedProduct(selected.getIdGroupList(), selectedProduct.getIdProduct(), 1))) {
-		    b = false;
+	/**
+	 * Return, if created the ListManager instance. Else, instantiate ListManager
+	 * and return it.
+	 * 
+	 * @return ListManager
+	 */
+	public static ListManager getInstance() {
+		if (instance == null) {
+			instance = new ListManager();
 		}
-	    } else {
-		QuantifiedProduct prod = res.get(0);
-		prod.setQuantity(prod.getQuantity() + 1);
-		if (!dao.update(prod)) {
-		    b = false;
+		return instance;
+	}
+
+	/**
+	 * Return the selected GroupList
+	 * 
+	 * @return GroupList
+	 */
+	public GroupList getSelected() {
+		return selected;
+	}
+
+	/**
+	 * Set the selected GroupList
+	 * 
+	 * @param selected GroupList
+	 */
+	public void setSelected(GroupList selected) {
+		this.selected = selected;
+	}
+	
+	public List<GroupList> getShoppingList(int id) {
+		DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
+		List<Couple> liste = new ArrayList<Couple>();
+		liste.add(new Couple("idGroup", Integer.toString(id)));
+		liste.add(new Couple("type", "S"));
+		return dao.get(liste);
+	}
+
+	public GroupList getFavoriteList(int id) {
+		DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
+		List<Couple> liste = new ArrayList<Couple>();
+		liste.add(new Couple("idGroup", Integer.toString(id)));
+		liste.add(new Couple("type", "F"));
+		return dao.get(liste).get(0);
+	}
+
+	public boolean addShopList(String name, int id) {
+		DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
+		return dao.save(new GroupList(0, id, name, new Date(), 'S'));
+	}
+
+	public boolean addFavorisList(int id) {
+		DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
+		return dao.save(new GroupList(0, id, "Favoris", new Date(), 'F'));
+	}
+
+	public boolean addSuggestList(int id) {
+		DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
+		return dao.save(new GroupList(0, id, "Suggest", new Date(), 'P'));
+	}
+
+	public boolean updateShopList(String name) {
+		DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
+		selected.setName(name);
+		return dao.update(selected);
+	}
+
+	public boolean removeList() {
+		DAO<GroupList> dao = AbstractDAOFactory.getInstance().getGroupListDAO();
+		return dao.delete(selected);
+	}
+
+	public String getNameSelected() {
+		return selected.getName();
+	}
+
+	public void getBoughtProducts(GroupList selected) {
+		DAO<PricedProduct> dao = AbstractDAOFactory.getInstance().getPricedProductDAO();
+		List<Couple> liste = new ArrayList<Couple>();
+		liste.add(new Couple("idGroupList", Integer.toString(selected.getIdGroupList())));
+		selected.setBoughtProducts(dao.get(liste));
+	}
+
+	public int getGroupListId() {
+		return this.selected.getIdGroupList();
+	}
+
+	public boolean addProductsToShopList(List<GeneralProduct> selectedProducts) {
+		DAO<QuantifiedProduct> dao = AbstractDAOFactory.getInstance().getQuantifiedProductDAO();
+		boolean b = true;
+		for (GeneralProduct selectedProduct : selectedProducts) {
+			List<Couple> liste = new ArrayList<Couple>();
+			liste.add(new Couple("idGroupList", Integer.toString(selected.getIdGroupList())));
+			liste.add(new Couple("idProduct", Integer.toString(selectedProduct.getIdProduct())));
+			List<QuantifiedProduct> res = dao.get(liste);
+			if (res.isEmpty()) {
+				if (!dao.save(new QuantifiedProduct(selected.getIdGroupList(), selectedProduct.getIdProduct(), 1))) {
+					b = false;
+				}
+			} else {
+				QuantifiedProduct prod = res.get(0);
+				prod.setQuantity(prod.getQuantity() + 1);
+				if (!dao.update(prod)) {
+					b = false;
+				}
+			}
 		}
-	    }
-	}
-	return b;
+		return b;
 
-    }
-
-    public void getShopList(GroupList selectedGroupList) {
-	DAO<QuantifiedProduct> dao = AbstractDAOFactory.getInstance().getQuantifiedProductDAO();
-	List<Couple> liste = new ArrayList<Couple>();
-	liste.add(new Couple("idGroupList", Integer.toString(selected.getIdGroupList())));
-	selected.setShoppinglist(dao.get(liste));
-    }
-
-    public void buyProduct(QuantifiedProduct p, Double price) {
-	DAO<QuantifiedProduct> dao = AbstractDAOFactory.getInstance().getQuantifiedProductDAO();
-	dao.delete(p);
-	DAO<PricedProduct> daoPP = AbstractDAOFactory.getInstance().getPricedProductDAO();
-	List<Couple> where = new ArrayList<Couple>();
-	where.add(new Couple("idGroupList", Integer.toString(p.getIdGroupList())));
-	where.add(new Couple("idProduct", Integer.toString(p.getIdProduct())));
-	List<PricedProduct> res = daoPP.get(where);
-	if (!res.isEmpty()) {
-	    PricedProduct product = res.get(0);
-	    Double prix = (price * p.getQuantity() + product.getQuantity() * product.getPrice()) / (p.getQuantity() + product.getQuantity());
-	    DecimalFormat df = new DecimalFormat("#.##");
-	    String aff = (df.format(prix)).replace(",", ".");
-	    prix = Double.parseDouble(aff);
-	    product.setPrice(prix);
-	    product.setQuantity(p.getQuantity() + product.getQuantity());
-	    daoPP.update(product);
-	} else {
-	    daoPP.save(new PricedProduct(price, p.getIdGroupList(), p.getIdProduct(), p.getQuantity()));
 	}
 
-    }
+	public void getShopList(GroupList selectedGroupList) {
+		DAO<QuantifiedProduct> dao = AbstractDAOFactory.getInstance().getQuantifiedProductDAO();
+		List<Couple> liste = new ArrayList<Couple>();
+		liste.add(new Couple("idGroupList", Integer.toString(selected.getIdGroupList())));
+		selected.setShoppinglist(dao.get(liste));
+	}
 
-    public void addOne(QuantifiedProduct p) {
-	DAO<QuantifiedProduct> dao = AbstractDAOFactory.getInstance().getQuantifiedProductDAO();
-	p.setQuantity(p.getQuantity() + 1);
-	dao.update(p);
-    }
+	public void buyProduct(QuantifiedProduct p, Double price) {
+		DAO<QuantifiedProduct> dao = AbstractDAOFactory.getInstance().getQuantifiedProductDAO();
+		dao.delete(p);
+		DAO<PricedProduct> daoPP = AbstractDAOFactory.getInstance().getPricedProductDAO();
+		List<Couple> where = new ArrayList<Couple>();
+		where.add(new Couple("idGroupList", Integer.toString(p.getIdGroupList())));
+		where.add(new Couple("idProduct", Integer.toString(p.getIdProduct())));
+		List<PricedProduct> res = daoPP.get(where);
+		if (!res.isEmpty()) {
+			PricedProduct product = res.get(0);
+			Double prix = (price * p.getQuantity() + product.getQuantity() * product.getPrice())
+			        / (p.getQuantity() + product.getQuantity());
+			DecimalFormat df = new DecimalFormat("#.##");
+			String aff = (df.format(prix)).replace(",", ".");
+			prix = Double.parseDouble(aff);
+			product.setPrice(prix);
+			product.setQuantity(p.getQuantity() + product.getQuantity());
+			daoPP.update(product);
+		} else {
+			daoPP.save(new PricedProduct(price, p.getIdGroupList(), p.getIdProduct(), p.getQuantity()));
+		}
 
-    public void removeOne(QuantifiedProduct p) {
-	DAO<QuantifiedProduct> dao = AbstractDAOFactory.getInstance().getQuantifiedProductDAO();
-	p.setQuantity(p.getQuantity() - 1);
-	dao.update(p);
-    }
+	}
 
-    public void remove(QuantifiedProduct p) {
-	DAO<QuantifiedProduct> dao = AbstractDAOFactory.getInstance().getQuantifiedProductDAO();
-	dao.delete(p);
-    }
+	public void addOne(QuantifiedProduct p) {
+		DAO<QuantifiedProduct> dao = AbstractDAOFactory.getInstance().getQuantifiedProductDAO();
+		p.setQuantity(p.getQuantity() + 1);
+		dao.update(p);
+	}
+
+	public void removeOne(QuantifiedProduct p) {
+		DAO<QuantifiedProduct> dao = AbstractDAOFactory.getInstance().getQuantifiedProductDAO();
+		p.setQuantity(p.getQuantity() - 1);
+		dao.update(p);
+	}
+
+	public void remove(QuantifiedProduct p) {
+		DAO<QuantifiedProduct> dao = AbstractDAOFactory.getInstance().getQuantifiedProductDAO();
+		dao.delete(p);
+	}
 
 }
